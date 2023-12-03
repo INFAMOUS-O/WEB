ab27133b639d285b3c296f8b7b80478ecfca7e51
victor <2236843@swansea.ac.uk>
2023-12-03 01:17:54 +0000

    created middleware for admin.. everything is still wroking

--- app/Http/Controllers/Homecontroller.php
+++ app/Http/Controllers/Homecontroller.php
@@ -1,11 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class Homecontroller extends Controller
 {
+    public  function index()
+    {
+        if(auth::id()){
+            $usertype=Auth()->user()->usertype;
+
+
+            if($usertype=='user')
+            {
+                return view('dashboard');
+            }
+
+            else if ($usertype=='admin')
+            {
+                return view('admin.admin-home');
+            }
+            else{
+                return redirect()->back();
+            }
+        }
+    }
+
     public  function homepage()
     {
         return view('home.homepage');
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [Homecontroller::class,'homepage']);
 
+Route::get('/home', [Homecontroller::class,'index'])->middleware('auth')->name('home');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
